cd9df7503e262096d7b7827c15500d2d00e4be88
telle000 <frialchristelleneri@gmail.com>
2024-11-19 08:46:14 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Welcome to the Kids Entertainment Zone!");
+    // Future interactive JavaScript code can be added here.
+  });
+  
